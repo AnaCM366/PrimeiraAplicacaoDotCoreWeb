4490b82205e5348794bd8522273a4ee1fab9c033
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-07-24 19:33:01 -0300

    Adicionar arquivos de projeto.

---
+++ Service/Db.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Crypto.Digests;
+
+namespace Service
+{
+    public class Db
+    { // ? = em algum momento a variável pode ser nula
+        private readonly MySqlConnection _con; // conexão
+        private MySqlCommand? _command; //comandos
+        private MySqlDataReader? _reader; //leitos de dados
+
+        public Db()
+        {
+            _con = new MySqlConnection("Server=127.0.0.1;Database=cadusuario;Uid=root;Pwd=SsMode=none;"); // Coloque aqui a string de conexão com o banco de dados
+        }
+
+        public List<UsuarioDTO> GetData() //
+        {
+            _con.Open(); //abrir conexão
+            _command = new MySqlCommand(); //criar comando
+            _command.Connection = _con; //atribuir conexão ao comando
+            _command.CommandText = "SELECT * FROM Usuario";
+            _reader = _command.ExecuteReader(); //executar comando e ler dados
+
+            List< UsuarioDTO> ListaDeUsuarios = new List<UsuarioDTO>();
+
+            while (_reader.Read())
+            {
+                var usuarioDTO = new UsuarioDTO()
+                {
+                    id = int.Parse(_reader["Id"].ToString()!),
+                    nome = _reader["Nome"].ToString()!,
+                    sobrenome = _reader["Sobrenome"].ToString()!,
+                    email = _reader["Email"].ToString()!
+                };
+                ListaDeUsuarios.Add(usuarioDTO); //adicionar usuário à lista
+            }
+            return ListaDeUsuarios;
+        }
+
+        public void AddUsuario(string nome)
+        {
+            _con.Open(); //abrir conexão
+            _command = new MySqlCommand(); //criar comando
+            _command.Connection = _con; //atribuir conexão ao comando
+
+            _command.CommandText = "INSERT INTO USUARIO (Nome, Sobrenome, Email) VALUES (?nome, ?sobrenome, ?email)";
+            _command.Parameters.Add("?nome", MySqlDbType.String).Value = nome;
+
+        }
+    }
+}
---
+++ Service/UsuarioDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class UsuarioDTO // Data Transfer Object (DTO) para o usuário
+    {
+        public int id;
+        public string nome = string.Empty;
+        public string sobrenome = string.Empty;
+        public string email = string.Empty;
+    }
+}
